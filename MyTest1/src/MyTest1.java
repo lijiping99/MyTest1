f18d6ecaa625b271e8ba095c8ca1c4654eb1999e
jiping.li <jiping.li@Corp-L-8172V09.adesa.com>
2019-03-27 15:14:29 -0400

    6

--- MyTest1/src/MyTest1.java
+++ MyTest1/src/MyTest1.java
@@ -1,7 +1,7 @@
 
 public class MyTest1 {
 	public static void main(String... args){
-		System.out.println("my my my my retest1...!");
+		System.out.println("my my my my my my retest1...!");
 	}
 
 }
