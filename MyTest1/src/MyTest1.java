a6731b9a5177b6855e9c9ec07995b8e5ddfe6673
Li <Jiping.Li@openlane.com>
2020-04-22 16:51:21 -0400

    change

--- MyTest1/src/MyTest1.java
+++ MyTest1/src/MyTest1.java
@@ -2,7 +2,7 @@
 public class MyTest1 {
 	public static void main(String... args){
 		System.out.println("my my my my my my retest1...!");
-		String message="1,2,3";
+		String message="1,2,3,5";
 		if(message.split(",").length>=3)
 			System.out.println(message);
 	}
