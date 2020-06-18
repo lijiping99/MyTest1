35623e3c50754094bfebe346f3d33b863eb68256
lijiping99 <42234916+lijiping99@users.noreply.github.com>
2020-06-18 13:51:36 -0400

    Update MyTest1.java

--- MyTest1/src/MyTest1.java
+++ MyTest1/src/MyTest1.java
@@ -4,7 +4,7 @@ public class MyTest1 {
 		System.out.println("my my my my my my retest1...!");
 		String message="1,2,3,4,5";
 		if(message.split(",").length>=3)
-			System.out.println(message);
+			System.out.println("message"=message);
 	}
 
 }
